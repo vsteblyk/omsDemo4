655d13d3432244ea1caebdb38d4976724b2e709e
Valentyn Steblyk <88666630+vsteblyk@users.noreply.github.com>
2021-12-08 16:52:46 +0200

    Delete src directory

--- src/main/java/com/softserveinc/edu/oms/domain/AbstractEntity.java
+++
@@ -1,7 +0,0 @@
-package com.softserveinc.edu.oms.domain;
-
-public abstract class AbstractEntity {
-	public abstract Integer getId();
-
-	public abstract void setId(final Integer id);
-}
--- src/main/java/com/softserveinc/edu/oms/persistence/dao/interfaces/IOrderStatusDao.java
+++
@@ -1,8 +0,0 @@
-package com.softserveinc.edu.oms.persistence.dao.interfaces;
-
-import com.softserveinc.edu.oms.domain.entities.OrderStatus;
-import com.softserveinc.edu.oms.persistence.dao.IDao;
-
-public interface IOrderStatusDao extends IDao<OrderStatus>{
-
-}
